597f6ba6a536345f4fd8d701b8d0ae097e197c7b
VALAKONDA KIRAN <129961117+KIRANVALAKONDA@users.noreply.github.com>
2025-08-23 20:48:10 +0530

    app.py

--- src/app.py
+++ src/app.py
@@ -6,5 +6,6 @@ app = Flask(_name_)
 def home():
     return jsonify({"message": "AI Resume Job Matcher API is running 🚀"})
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
