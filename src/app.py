1c1ea8b559d6e12378d27343e73828d683b8da1a
KIRANVALAKONDA <kirangoud7450@gmail.com>
2025-08-24 06:00:39 +0530

    Add sample data and basic preprocessing

--- src/app.py
+++ src/app.py
@@ -1,1 +1,12 @@
+HEAD
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "AI Resume Job Matcher API is running 🚀"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+cca6cc3 (Add sample data and basic preprocessing)
